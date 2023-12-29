2ef9c587417a4820e408f2282b4451f1e42064b7
Avigdor <avigdorpartouche@gmail.com>
2023-12-29 19:46:33 +0200

    update of map display

--- src/features/game/Game.js
+++ src/features/game/Game.js
@@ -15,8 +15,10 @@ const generateTiles = (width, height, freq = 1) => {
 
       for (let i = 0; i < 8; i++) {
         val +=
-          noise2D((x * frequency) / width, (y * frequency) / height) *
-          amplitude;
+          noise2D(
+            (y * frequency * width) / height / width,
+            (x * frequency) / height
+          ) * amplitude;
 
         frequency *= 2;
         amplitude /= 2;
@@ -27,47 +29,100 @@ const generateTiles = (width, height, freq = 1) => {
   }
 };
 
-generateTiles(300, 300, 1);
+generateTiles(300, 150, 1);
 
 const Game = (props) => {
   const [noise, setNoise] = useState([]);
+  const [width, setWidth] = useState(4);
+  const [height, setHeigth] = useState(4);
+
+  // window.addEventListener("keydown", (e) => {
+  //   switch (e.key) {
+  //     case "z":
+  //     case "ArrowUp":
+  //       break;
+  //     case "s":
+  //     case "ArrowDown":
+  //       break;
+  //     case "q":
+  //     case "ArrowLet":
+  //       break;
+  //     case "d":
+  //     case "ArrowRight":
+  //       break;
+  //     case "+":
+  //       setWidth(width + 2);
+  //       setHeigth(height + 2);
+  //       console.log("+");
+  //       break;
+  //     case "-":
+  //       setWidth(width - 2);
+  //       setHeigth(height - 2);
+  //       console.log("-");
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // });
 
   useEffect(() => {
     setNoise(tiles);
   }, []);
+  console.log("bip");
 
   const tileColor = (val) => {
     if (val < -0.45) {
       return "blue";
     } else if (val >= -0.45 && val < -0.25) {
-      return "rgb(100,100,255)";
-    } else if (val >= -0.25 && val < -0.15) {
+      return "rgb(64,92,255)";
+    } else if (val >= -0.25 && val < -0.1) {
       return "yellow";
-    } else if (val >= -0.15 && val < 0.66) {
+    } else if (val >= -0.1 && val < 0.75) {
       return "rgb(0,192,0)";
-    } else if (val >= 0.66 && val < 0.9) {
+    } else if (val >= 0.75 && val < 0.95) {
       return "grey";
-    } else if (val >= 0.9) {
+    } else if (val >= 0.95) {
       return "white";
     }
   };
 
   return (
     <>
-      {noise.map((y, i) => {
-        return (
-          <div>
-            <br />
-            {y.map((x, j) => (
-              <div
-                key={i + 1 + j}
-                className="tile"
-                style={{ backgroundColor: tileColor(x) }}
-              ></div>
-            ))}
-          </div>
-        );
-      })}
+      <div
+        className="grid"
+        style={{
+          gridTemplateColumns: "repeat(300, 1fr)",
+          gridTemplateRows: "repeat(150, 1fr)",
+        }}
+      >
+        {noise.map((y, i) => {
+          return (
+            <>
+              {y.map((x, j) => (
+                <div
+                  key={i + 1 + j}
+                  style={{
+                    backgroundColor: tileColor(x),
+                    width: width + "px",
+                    height: height + "px",
+                  }}
+                ></div>
+              ))}
+            </>
+          );
+        })}
+        <div
+          style={{
+            backgroundColor: "black",
+            width: "2px",
+            height: "2px",
+            top: "calc(((100vh - 600px) / 2) + 201px )",
+            left: "calc(((100vw - 1200px) / 2) + 201px)",
+            position: "absolute",
+            borderRadius: "4px",
+          }}
+        ></div>
+      </div>
     </>
   );
 };
